8984fd15545b8b8f9827584dab11b02976657a1d
weiving <cww854148458>
2018-03-15 18:51:32 +0800

    消息

---
+++ src/components/common/pagination.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="paging">
+      <template></template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "paging"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/openAccount/shareQartCode.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "share-qart-code"
+    }
+</script>
+
+<style scoped>
+
+</style>
